9a0d10c44a916129b1159faa39bd76f93b250dfc
aphisart <133872460+aphisart@users.noreply.github.com>
2023-05-17 19:00:58 +0300

    Add files via upload

---
+++ MOS/MOS/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace MOS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButtonLoadPicture = new System.Windows.Forms.ToolStripButton();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButtonLoadPicture});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(984, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButtonLoadPicture
+            // 
+            this.toolStripButtonLoadPicture.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButtonLoadPicture.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonLoadPicture.Image")));
+            this.toolStripButtonLoadPicture.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonLoadPicture.Name = "toolStripButtonLoadPicture";
+            this.toolStripButtonLoadPicture.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButtonLoadPicture.Text = "toolStripButton1";
+            this.toolStripButtonLoadPicture.Click += new System.EventHandler(this.toolStripButtonLoadPicture_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(50, 50);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 300);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(350, 50);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(300, 300);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(650, 50);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(300, 300);
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(650, 350);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(300, 300);
+            this.pictureBox4.TabIndex = 6;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Location = new System.Drawing.Point(350, 350);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(300, 300);
+            this.pictureBox5.TabIndex = 5;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Location = new System.Drawing.Point(50, 350);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(300, 300);
+            this.pictureBox6.TabIndex = 4;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Location = new System.Drawing.Point(650, 650);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(300, 300);
+            this.pictureBox7.TabIndex = 9;
+            this.pictureBox7.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Location = new System.Drawing.Point(350, 650);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(300, 300);
+            this.pictureBox8.TabIndex = 8;
+            this.pictureBox8.TabStop = false;
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.Location = new System.Drawing.Point(50, 650);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(300, 300);
+            this.pictureBox9.TabIndex = 7;
+            this.pictureBox9.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 961);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox9);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButtonLoadPicture;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox pictureBox9;
+    }
+}
+
---
+++ MOS/MOS/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MOS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        PictureBox[] pbSegments;
+        Bitmap Picture;
+        int numRect = 9;
+        EventHandler PB_Click;
+
+        private void toolStripButtonLoadPicture_Click(object sender, EventArgs e)
+        {
+            LoadPicture();
+        }
+        private void LoadPicture()
+        {
+            var ofDlg = new OpenFileDialog();
+
+            // Фильтр показа изображений с определенным расширением.
+            ofDlg.Filter = "файлы картинок (*.bmp;*.jpg;*.jpeg;*.gif;)|";
+            ofDlg.Filter += "*.bmp;*.jpg;**.png;*.jpeg;*.gif|All files (*.*)|*.*";
+            ofDlg.FilterIndex = 1;
+            ofDlg.RestoreDirectory = true;
+
+            if (ofDlg.ShowDialog() == DialogResult.OK)
+            {
+                // Загружаем выбранную картинку.
+                Picture = new Bitmap(ofDlg.FileName);
+
+                // Создание сегментов
+                CreatePictureSegments();
+            }
+        }
+        private void CreatePictureSegments()
+{
+    // Удалим предыдущий массив, чтобы создать новый.
+    if (pbSegments != null)
+    {
+        for (int i = 0; i < pbSegments.Length; i++)
+        {
+            pbSegments[i].Dispose();
+        }
+        pbSegments = null;
+    }
+
+
+    // Создаем массив прямоугольников установленного размера.
+    pbSegments = new PictureBox[numRect * numRect];
+
+    // Вычислим габаритные размеры прямоугольников.
+    int w = ClientSize.Width / numRect;
+    int h = ClientSize.Height / numRect;
+
+    // Счетчики порядкового номера по координатам Х и Y.
+    int countX = 0;
+    int countY = 0;
+
+    for (int i = 0; i < pbSegments.Length; i++)
+    {
+        // Размеры и координаты размещения созданного прямоугольника.
+        pbSegments[i] = new PictureBox
+        {
+            Width = w,
+            Height = h,
+            Left = countX * w,
+            Top = countY * h
+        };
+
+
+        // Запомним начальные координаты прямоугольника для
+        // восстановления перемешанной картинки,
+        // и определения полной сборки картинки.
+        Point pt = new Point();
+        pt.X = pbSegments[i].Left;
+        pt.Y = pbSegments[i].Top;
+
+        // сохраним координаты в свойстве Tag каждого прямоугольника
+        pbSegments[i].Tag = pt;
+
+        // Считаем прямоугольники по рядам и столбцам.
+        countX++;
+        if (countX == numRect)
+        {
+            countX = 0;
+            countY++;
+        }
+
+
+        pbSegments[i].Parent = this;
+        pbSegments[i].BorderStyle = BorderStyle.None;
+        pbSegments[i].SizeMode = PictureBoxSizeMode.StretchImage;
+
+        // Новые сегменты должны быть все видимы.
+        pbSegments[i].Show();
+
+
+        // Для всех прямоугольников массива событие клика мыши
+        // будет обрабатываться в одной и том же методе.
+        pbSegments[i].Click += new EventHandler(PB_Click);
+
+    }// for (int i = 0; i < pbSegments.Length; i++)
+
+    DrawPicture();
+}
+        private void DrawPicture()
+        {
+            if (Picture == null) return;
+
+            int countX = 0;
+            int countY = 0;
+
+            for (int i = 0; i < pbSegments.Length; i++)
+            {
+                int w = Picture.Width / numRect;
+                int h = Picture.Height / numRect;
+                pbSegments[i].Image =
+                    Picture.Clone(new RectangleF(countX * w, countY * h, w, h),
+                        Picture.PixelFormat);
+                countX++;
+                if (countX == numRect)
+                {
+                    countX = 0;
+                    countY++;
+                }
+            }
+        }
+        private void CorrectSizeSegments()
+        {
+            if (pbSegments == null) return;
+
+            // Предыдущие размеры сегментов
+            int oldwidth = pbSegments[0].Width;
+            int oldheight = pbSegments[0].Height;
+
+
+            // Новые размеры прямоугольников.
+            int w = ClientSize.Width / numRect;
+            int h = ClientSize.Height / numRect;
+
+
+
+            //int countX = 0; // счетчик прямоугольников по координате X в одном ряду
+            //int countY = 0; // счетчик прямоугольников по координате Y в одном столбце
+            for (int i = 0; i < pbSegments.Length; i++)
+            {
+                pbSegments[i].Width = w;
+                pbSegments[i].Height = h;
+
+                // Получим порядковый номер сегмента по координате Х
+                int countX = pbSegments[i].Left /= oldwidth;
+
+                // Получим порядковый номер сегмента по координате Y
+                int countY = pbSegments[i].Top /= oldheight;
+
+                pbSegments[i].Left = countX * w;
+                pbSegments[i].Top = countY * h;
+            }
+        }
+        private void MixedSegments()
+        {
+            if (Picture == null) return;
+
+            // Создаем объект генерирования псевдослучайных чисел,
+            // для различного набора случайных чисел инициализацию
+            // объекта Random производим от счетчика количества
+            // миллисекунд прошедших со времени запуска операционной системы.
+            Random rand = new Random(Environment.TickCount);
+            for (int i = 0; i < pbSegments.Length; i++)
+            {
+                pbSegments[i].Visible = true;
+                int temp = rand.Next(0, pbSegments.Length);
+                Point ptR = pbSegments[temp].Location;
+                Point ptI = pbSegments[i].Location;
+                pbSegments[i].Location = ptR;
+                pbSegments[temp].Location = ptI;
+
+                // Бордюр чтобы видно было прямоугольники
+                pbSegments[i].BorderStyle = BorderStyle.Fixed3D;
+            }
+
+            // Случайным образом выбираем пустой прямоугольник,
+            // делаем его невидимым.
+            int r = rand.Next(0, pbSegments.Length);
+            pbSegments[r].Visible = false;
+        }
+
+
+
+    }
+}
