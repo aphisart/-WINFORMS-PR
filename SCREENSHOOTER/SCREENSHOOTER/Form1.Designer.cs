43c7140afce471903ffcd4e728c7dc3b2410aa9b
aphisart <133872460+aphisart@users.noreply.github.com>
2023-05-17 19:03:56 +0300

    Add files via upload

---
+++ SCREENSHOOTER/SCREENSHOOTER/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace SCREENSHOOTER
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonShot = new System.Windows.Forms.Button();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonShot
+            // 
+            this.buttonShot.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonShot.Location = new System.Drawing.Point(0, 0);
+            this.buttonShot.Name = "buttonShot";
+            this.buttonShot.Size = new System.Drawing.Size(284, 126);
+            this.buttonShot.TabIndex = 0;
+            this.buttonShot.Text = "СДЕЛАТЬ СКРИНШОТ";
+            this.buttonShot.UseVisualStyleBackColor = true;
+            this.buttonShot.Click += new System.EventHandler(this.buttonShot_Click);
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.buttonExit.Location = new System.Drawing.Point(0, 124);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(284, 137);
+            this.buttonExit.TabIndex = 1;
+            this.buttonExit.Text = "ВЫЙТИ";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.buttonShot);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonShot;
+        private System.Windows.Forms.Button buttonExit;
+    }
+}
+
